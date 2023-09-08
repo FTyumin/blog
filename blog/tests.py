49937ca84a073e37a1c260ef96f60dcbb2dfc630
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-09-08 16:54:26 +0300

    Small edit

--- blog/tests.py
+++ blog/tests.py
@@ -8,15 +8,11 @@ class BlogTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create_user(
-            username = "testuser", email = "test@email.com", password= "secret"
+        username="testuser", email="test@email.com", password="secret"
         )
-
         cls.post = Post.objects.create(
-            title = "A good title",
-            body = "Nice body content",
-            author = cls.user
+        title="A good title", body="Nice body content", author=cls.user,
         )
-
     def test_post_model(self):
         self.assertEqual(self.post.title, "A good title")
         self.assertEqual(self.post.body, "Nice body content")
@@ -24,26 +20,28 @@ class BlogTests(TestCase):
         self.assertEqual(str(self.post), "A good title")
         self.assertEqual(self.post.get_absolute_url(), "/post/1/")
 
-    def test_url_exists_at_correct_location_listview(self):
+    def test_url_exists_at_correct_location_listview(self): # new
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_exists_at_correct_location_detailview(self):
-        response = self.client.get("/")
+    def test_url_exists_at_correct_location_detailview(self): # new
+        response = self.client.get("/post/1/")
         self.assertEqual(response.status_code, 200)
 
-    def test_post_listview(self):
+    def test_post_listview(self): # new
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Nice body content")
         self.assertTemplateUsed(response, "home.html")
 
-    def test_post_detailView(self):
+    def test_post_detailview(self): # new
         response = self.client.get(reverse("post_detail",
-            kwargs={"pk":self.post.pk}))
-        no_response = self.client.get("/post/1", follow=True)
+        kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("/post/100000/")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "A good title")
         self.assertTemplateUsed(response, "post_detail.html")
+
+
     
