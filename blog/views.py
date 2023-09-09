d8f393ec955e88f69e9c986d92669d9e790326a2
FTyumin <71492298+FTyumin@users.noreply.github.com>
2023-09-09 23:05:56 +0300

    Upadte & Delete fucntions

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -14,3 +15,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name ="post_delete.html"
+    success_url = reverse_lazy("home")
